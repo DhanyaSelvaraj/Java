5e86872de969f8213d80987c4db26053b65d1388
DhanyaSelvaraj <34230161+DhanyaSelvaraj@users.noreply.github.com>
2018-02-01 10:50:17 +0530

    Add files via upload

--- demothread.java
+++ demothread.java
@@ -3,18 +3,19 @@ class sample extends Thread
 	Thread t=new Thread(this);
 	sample()
 	{
-		System.out.println("Hiii "+t.getName());
-	}	
+		t.start();
+	
+		System.out.println(t.getName());
+	}
 	public void run()
 	{
 		for(int i=0;i<10;i++)
 		{
-			System.out.println(i);
 			try{
-				t.sleep(1000);
-			}
-			catch(InterruptedException d){}
-		}	
+				t.sleep(1000);	
+			}catch(Exception e){}
+			System.out.println("Child thread: "+i);
+		}
 	}
 }
 class demothread
@@ -22,16 +23,6 @@ class demothread
 	public static void main(String args[])
 	{
 		sample obj=new sample();
-		obj.start();
 		
-		Thread t=Thread.currentThread();
-		
-		try{
-			obj.join();
-		}
-		catch(InterruptedException dd){}
-		
-		sample obj1=new sample();
-		obj1.start();
 	}
 }
